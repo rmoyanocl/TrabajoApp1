dd86428f8d495745609f3309c48a0d07c47b13d6
rmoyanocl <rmoyano@rubenmoyano.cl>
2014-03-24 12:57:16 -0700

    Update FormMesas.cs

--- FormMesas.cs
+++ FormMesas.cs
@@ -18,13 +18,17 @@ namespace trabajo_app_1
 
         private void buttonMesa1_Click(object sender, EventArgs e)
         {
+
+            /*
+            //Cambiar estado del boton mesa dependiendo del item seleccionado en el combobox
+            *
             if(comboBoxMesa1.SelectedItem.Equals("Libre"))
             {
                 this.buttonMesa1.BackColor = Color.Green;
             }
             if (comboBoxMesa1.SelectedItem.Equals("Reservada"))
             {
-                this.buttonMesa1.BackColor = Color.DarkOrange;
+                this.buttonMesa1.BackColor = Color.Brown;
             }
             if (comboBoxMesa1.SelectedItem.Equals("Ocupada"))
             {
@@ -32,15 +36,167 @@ namespace trabajo_app_1
             }
             else
             {
-                this.buttonMesa1.BackColor = Color.DarkSeaGreen;
+                return;
+            }
+            * 
+            */
+
+
+            if (radioButtonLibre1.Checked == true)
+            {
+
+                DialogResult resultLibre1 = MessageBox.Show("Liberar esta mesa?",
+                "Mesas Disponibles",
+                MessageBoxButtons.YesNo);
+
+                if (resultLibre1 == DialogResult.Yes)
+                {
+                    this.buttonMesa1.BackColor = Color.LawnGreen;
+                }
+                else
+                {
+                    return;
+                }
+            }
+            if (radioButtonOcupada1.Checked == true)
+            {
+                DialogResult resultOcupada1 = MessageBox.Show("Ocupar esta mesa?",
+                "Mesas Disponibles",
+                MessageBoxButtons.YesNo);
+
+                if (resultOcupada1 == DialogResult.Yes)
+                {
+                    this.buttonMesa1.BackColor = Color.Red;
+                }
+                else
+                {
+                    return;
+                }
+            }
+            if (radioButtonReservada1.Checked == true)
+            {
+                DialogResult resultReservada1 = MessageBox.Show("Reservar esta mesa?",
+                "Mesas Disponibles",
+                MessageBoxButtons.YesNo);
+
+                if (resultReservada1 == DialogResult.Yes)
+                {
+                    this.buttonMesa1.BackColor = Color.Cyan;
+                }
+                else
+                {
+                    return;
+                }   
+            }
+            else
+            {
+                return;
             }
         }
 
 
 
-        private void comboBoxMesa1_SelectedIndexChanged(object sender, EventArgs e)
-        {
 
+
+        private void buttonMesa2_Click(object sender, EventArgs e)
+        {
+            if (radioButtonLibre2.Checked == true)
+            {
+                this.buttonMesa2.BackColor = Color.LawnGreen;
+            }
+            if (radioButtonOcupada2.Checked == true)
+            {
+                this.buttonMesa2.BackColor = Color.Red;
+            }
+            if (radioButtonReservada2.Checked == true)
+            {
+                this.buttonMesa2.BackColor = Color.Cyan;
+            }
+            else
+            {
+                return;
+            }
         }
+
+        private void buttonMesa3_Click(object sender, EventArgs e)
+        {
+            if (radioButtonLibre3.Checked == true)
+            {
+                this.buttonMesa3.BackColor = Color.LawnGreen;
+            }
+            if (radioButtonOcupada3.Checked == true)
+            {
+                this.buttonMesa3.BackColor = Color.Red;
+            }
+            if (radioButtonReservada3.Checked == true)
+            {
+                this.buttonMesa3.BackColor = Color.Cyan;
+            }
+            else
+            {
+                return;
+            }
+        }
+
+        private void buttonMesavip1_Click(object sender, EventArgs e)
+        {
+            if (radioButtonLibreVip1.Checked == true)
+            {
+                this.buttonMesavip1.BackColor = Color.LawnGreen;
+            }
+            if (radioButtonOcupadaVip1.Checked == true)
+            {
+                this.buttonMesavip1.BackColor = Color.Red;
+            }
+            if (radioButtonReservadaVip1.Checked == true)
+            {
+                this.buttonMesavip1.BackColor = Color.Cyan;
+            }
+            else
+            {
+                return;
+            }
+        }
+
+        private void buttonMesavip2_Click(object sender, EventArgs e)
+        {
+            if (radioButtonLibreVip2.Checked == true)
+            {
+                this.buttonMesavip2.BackColor = Color.LawnGreen;
+            }
+            if (radioButtonOcupadaVip2.Checked == true)
+            {
+                this.buttonMesavip2.BackColor = Color.Red;
+            }
+            if (radioButtonReservadaVip2.Checked == true)
+            {
+                this.buttonMesavip2.BackColor = Color.Cyan;
+            }
+            else
+            {
+                return;
+            }
+        }
+
+        private void buttonMesavip3_Click(object sender, EventArgs e)
+        {
+            if (radioButtonLibreVip3.Checked == true)
+            {
+                this.buttonMesavip3.BackColor = Color.LawnGreen;
+            }
+            if (radioButtonOcupadaVip3.Checked == true)
+            {
+                this.buttonMesavip3.BackColor = Color.Red;
+            }
+            if (radioButtonReservadaVip3.Checked == true)
+            {
+                this.buttonMesavip3.BackColor = Color.Cyan;
+            }
+            else
+            {
+                return;
+            }
+        }
+
     }
 }
